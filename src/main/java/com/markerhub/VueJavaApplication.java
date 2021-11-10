c112db9fbca7632039d83c83ad085d4f4fa61616
jzdingwenlong <jzdingwenlong@qq.com>
2021-11-10 17:25:53 +0800

    初始化项目

---
+++ src/main/java/com/markerhub/VueJavaApplication.java
@@ -0,0 +1,13 @@
+package com.markerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueJavaApplication.class, args);
+    }
+
+}
